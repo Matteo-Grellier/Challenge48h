e62ead6eebf348f5a5ff770ecb5668255bc5323d
Fquicy <florien.quicy@ynov.com>
2023-02-06 19:06:24 +0100

    export function

--- app/app.js
+++ app/app.js
@@ -1,38 +1,13 @@
 //Importing modules
 const express = require('express')
-const mysql = require('mysql')
 const app = express()
 const port = 3000
 
-// Create a connection to the database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: "marketplace"       
-})
+const { func } = require('./request.js')
 
-// open the MySQL connection
-connection.connect(error => {
-    if (error){
-        console.log("A error has been occurred "
-            + "while connecting to database.")   
-        throw error
-    }
-})
-
+// simple route
 app.get('/', (req, res) => {
-    try {
-        var sql = connection.query("SELECT * FROM products", (err, result) => {
-            if (err) throw err;
-            console.log(result);
-            res.send(result)
-        })
-    }
-    catch (error) {
-        console.log("A error has been occurred ")
-        throw error
-    }
+    func(req,res)
 })
 
 app.listen(port, ()=>{
--- app/request.js
+++ app/request.js
@@ -1,2 +1,37 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql')
+
+// Create a connection to the database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'marketplace'     
+})
+
+// open the MySQL connection
+connection.connect(error => {
+    if (error){
+        console.log("A error has been occurred "
+            + "while connecting to database.")   
+        throw error
+    }
+})
+
+
+func = function(req, res){
+    try {
+        var sql = connection.query("SELECT * FROM products", (err, result) => {
+            if (err) throw err
+            console.log(result)
+            res.send(result)
+        })
+    }
+    catch (error) {
+        console.log("A error has been occurred ")
+        throw error
+    }
+}
+
+module.exports = { func }
