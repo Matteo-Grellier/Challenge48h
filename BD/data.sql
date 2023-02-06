a1198339e34b73549d836197b3bc88b0117c0462
arthu <arthur03moulin@gmail.com>
2023-02-06 15:50:08 +0100

    modifications BD pour inclure catégories et mails

--- BD/data.sql
+++ BD/data.sql
@@ -1,30 +1,29 @@
-INSERT INTO merchants (merchant_name, admin, blacklist)
+INSERT INTO categories (category_name)
 VALUES
-  ('Merchant 1', 1, 0),
-  ('Merchant 2', 0, 0),
-  ('Merchant 3', 0, 1);
+  ('Plushies'),
+  ('Furnitures'),
+  ('Food');
 
-INSERT INTO clients (full_name, country_code, address)
+INSERT INTO merchants (merchant_name, admin, blacklist, email, password)
 VALUES
-  ('John Doe', 1, '123 Main St'),
-  ('Jane Doe', 2, '456 Main St'),
-  ('Jim Smith', 3, '789 Main St');
+  ('Merchant 1', 1, 0, 'merchant1@email.com', 'password1'),
+  ('Merchant 2', 0, 0, 'merchant2@email.com', 'password2'),
+  ('Merchant 3', 0, 1, 'merchant3@email.com', 'password3');
 
-INSERT INTO products (seller_id, product_name, price, picture)
+INSERT INTO clients (full_name, country_code, address, email, password)
 VALUES
-  (1, 'Product 1', 10.99, 'product1.jpg'),
-  (2, 'Product 2', 12.99, 'product2.jpg'),
-  (3, 'Product 3', 14.99, 'product3.jpg');
+  ('John Doe', 'USA', '123 Main St', 'johndoe@email.com', 'password1'),
+  ('Jane Doe', 'CAN', '456 Main St', 'janedoe@email.com', 'password2'),
+  ('Jim Smith', 'GBR', '789 Main St', 'jimsmith@email.com', 'password3');
+
+INSERT INTO products (seller_id, product_name, price, picture, category_id)
+VALUES
+  (1, 'Fortnite Burger', 10.99, 'product1.jpg', 1),
+  (2, 'Blahaj', 12.99, 'product2.jpg', 2),
+  (3, 'Hampter', 14.99, 'product3.jpg', 3);
 
 INSERT INTO orders (buyer_id, product_id)
 VALUES
   (1, 1),
   (2, 2),
   (3, 3);
-
-  
-INSERT INTO categories (category_name)
-VALUES
-  ('Food'),
-  ('Books'),
-  ('Furnitures');
