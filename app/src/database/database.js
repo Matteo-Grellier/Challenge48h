a977822ea168dd0b33bb2aa6c6899113b25fe5b3
matteo-grellier <matteogrellier@gmail.com>
2023-02-07 15:24:27 +0100

    Update API

---
+++ app/src/database/database.js
@@ -0,0 +1,30 @@
+import pkg from 'pg';
+import { createTablesQuery } from './createTables.js';
+const { Pool } = pkg;
+
+const pool = new Pool({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'marketplace',
+  port: 5432
+})
+
+const execute = async (query) => {
+  try {
+      await pool.connect();     // gets connection
+      await pool.query(query);  // sends queries
+      return true;
+  } catch (error) {
+      console.error(error.stack);
+      return false;
+  }
+};
+
+execute(createTablesQuery).then(result => {
+  if (result) {
+      console.log('Tables created');
+  }
+});
+
+export default pool;
