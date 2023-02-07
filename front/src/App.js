1281d0e12e7a24371de4b3443398c0b7be73c920
letienne <etienne.lemee@ynov.com>
2023-02-07 10:07:45 +0100

    made each card diffrent from the others

--- front/src/App.js
+++ front/src/App.js
@@ -5,44 +5,34 @@ import Header from "./components/Header.js";
 function App() {
   return (
     <div>
-      <meta charSet="UTF-8" />
-      <title>Document</title>
-      <link rel="stylesheet" href="../assets/homePage.css" type="text/css" />
-      <header>
-        <Header></Header>
-      </header>
-      <body>
-
-        <div className="content">
-          <div className="contentcard">
-            <Card> <h4> test</h4></Card>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-
-
-          </div>
-        </div>
-
-
-      </body>
+    <meta charSet="UTF-8" />
+    <title>Document</title>
+    <link rel="stylesheet" href="../assets/homePage.css" type="text/css" />
+    <header>
+    <Header></Header>
+    </header>
+    <body>
+    
+    <div className="content">
+    <div className="contentcard">
+    <Card title= "test" price="125" sellerName="Bob"/>
+    <Card title= "test" price="125" sellerName="Bob"/>
+    <Card title= "test" price="125" sellerName="Bob"/>
+    <Card title= "test" price="125" sellerName="Bob"/>
+    <Card title= "test" price="125" sellerName="Bob"/>
+    <Card title= "test" price="125" sellerName="Bob"/>
+    <Card title= "test" price="125" sellerName="Bob"/>
+    <Card title= "test" price="125" sellerName="Bob"/>
+    <Card title= "test" price="125" sellerName="Bob"/>
+    <Card title= "test" price="125" sellerName="Bob"/>
+    <Card title= "test" price="125" sellerName="Bob"/>
+    <Card title= "test" price="125" sellerName="Bob"/>
     </div>
-
-  );
-}
-
-export default App;
+    </div>
+    </body>
+    </div>
+    
+    );
+  }
+  
+  export default App;
--- front/src/components/Card.js
+++ front/src/components/Card.js
@@ -1,4 +1,4 @@
-function Card() {
+function Card({title,price,sellerName}) {
     return (
         
         <div className="card">
@@ -6,9 +6,9 @@ function Card() {
         src="https://images.pexels.com/photos/51312/kiwi-fruit-vitamins-healthy-eating-51312.jpeg?cs=srgb&dl=pexels-pixabay-51312.jpg&fm=jpg"
         style={{width: '100%' }} />
         <div className="container">
-        <h4><b>nom du produit</b></h4>  
-        <p>prix$$$</p>
-        <p>nom du vendeur </p>
+        <h4><b>{title}</b></h4>  
+        <p>{price}</p>
+        <p>{sellerName}</p>
         </div>
         </div>
         
