68a832b03149b33283b195475a43d038961e4a95
Nathan BOURRY <nathan.bourry@ynov.com>
2023-02-07 08:46:26 +0100

    beging routes

--- app/app.js
+++ app/app.js
@@ -25,23 +25,7 @@ app.post('/client/login', async (req, res) => {
     }
 })
 
-app.post('/marchant/login', async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        const user = await User.findOne({ email });
-        if (!user) return res.code(401).send({ message: 'ERR_INVALID_LOGIN' });
-
-        if (!await bcrypt.compare(password, user.password)) return res.code(401).send()
-
-        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
-
-        return res.code(200).send({ token });
-    } catch (error) {
-        return res.code(401).send({ message: error.message });
-    }
-})
-
-app.post('register', async (req, res) => {
+app.post('/client/register', async (req, res) => {
     const { firstname, lastname, email, password } = req.body; // A CHANGER
         try {
             const user = new User({
@@ -60,7 +44,60 @@ app.post('register', async (req, res) => {
 
 })
 
-app.post('/product', (req, res) => { //A FAIRe
+
+app.post('/marchant/login', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) return res.code(401).send({ message: 'ERR_INVALID_LOGIN' });
+
+        if (!await bcrypt.compare(password, user.password)) return res.code(401).send()
+
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+
+        return res.code(200).send({ token });
+    } catch (error) {
+        return res.code(401).send({ message: error.message });
+    }
+})
+
+app.post('/marchant/register', async (req, res) => {
+    const { firstname, lastname, email, password } = req.body; // A CHANGER
+        try {
+            const user = new User({
+                firstname,
+                lastname,
+                email,
+                password: await bcrypt.hash(password, 10)
+            });
+
+            await user.save();
+
+            return res.code(201).send()
+        } catch (error) {
+            return res.code(400).send(error);
+        }
+
+})
+
+app.post('/product/add', (req, res) => { //A FAIRe
+
+})
+
+app.get('/product', (req, res) => {
+
+})
+
+app.delete('/product/:id', (req, res) => {
+    
+})
+
+app.post('/product/order', (req, res) => {
+
+})
+
+
+app.get('/order/', (req, res) => {
 
 })
 
